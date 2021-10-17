b0f77332a3b7b8830d74e09f98dd7a64538bc7d9
Krishana Dave <krishnadave671@gmail.com>
2021-10-17 16:27:18 +0530

    maths class created

---
+++ Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Roll no is" + args[0]);
+        System.out.println("name is " + args[1]);
+    }
+}
