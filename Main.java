72f56d8a30401530d2d437a7a03fc688b2815659
Krishana Dave <krishnadave671@gmail.com>
2021-10-17 16:33:31 +0530

    sum.class added

--- Main.java
+++ Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Roll no is" + args[0]);
         System.out.println("name is " + args[1]);
+        // prajwal
     }
 }
